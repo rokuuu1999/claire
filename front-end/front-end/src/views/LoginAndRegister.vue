b7494fb5e5b13b95752aaa124005eb151598372e
Hanmengnan <1448189829@qq.com>
2021-02-06 17:51:22 +0800

    front first

---
+++ front-end/front-end/src/views/LoginAndRegister.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "LoginAndRegister",
+  data() {
+    return {}
+  },
+  method: {
+    login: function () {
+
+    },
+    register: function () {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
