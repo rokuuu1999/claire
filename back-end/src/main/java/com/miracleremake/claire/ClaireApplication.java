79ee1ec65e7230d0a6a4121dee9b7e010262971e
rokuuu1999 <rokuuu1999@gmail.com>
2021-08-11 23:22:10 +0800

    third commit

---
+++ back-end/src/main/java/com/miracleremake/claire/ClaireApplication.java
@@ -0,0 +1,1 @@
+package com.miracleremake.claire;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplicationpublic class ClaireApplication {    public static void main(String[] args) {        SpringApplication.run(ClaireApplication.class, args);    }}
---
+++ back-end/src/test/java/com/miracleremake/claire/ClaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miracleremake.claire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
